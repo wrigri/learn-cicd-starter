0e7948ee7c27b224067efb6b452b548b4a97f3d0
wrigri <rickw211@gmail.com>
2023-07-17 23:59:15 -0700

    Add some tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,47 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type test struct {
+		input  http.Header
+		outStr string
+		outErr string
+	}
+
+	tests := []test{
+		{
+			input:  http.Header{"Authorization": []string{"ApiKey MyAPIKey"}},
+			outStr: "MyAPIKey",
+			outErr: "",
+		},
+		{
+			input:  http.Header{"Authorization": []string{"NotApiKey MyAPIKey"}},
+			outStr: "",
+			outErr: "malformed authorization header",
+		},
+		{
+			input:  http.Header{"Other": []string{"ApiKey MyAPIKey"}},
+			outStr: "",
+			outErr: "no authorization header included",
+		},
+		
+	}
+
+	for _, tc := range tests {
+		fmt.Println(tc.input.Get("Authorization"))
+		got, err := GetAPIKey(tc.input)
+		errStr := ""
+		if err != nil {
+			errStr = err.Error()
+		}
+
+		if got != tc.outStr || errStr != tc.outErr {
+			t.Fatalf("expected (error): %v (%v), got (error): %v (%v)", tc.outStr, tc.outErr, got, err)
+		}
+	}
+}
